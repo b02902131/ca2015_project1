d8199a162d73057615938d8f8c925b220e61bbee
Derek Kuo <ccderek@bitbucket.org>
2015-12-05 00:55:44 +0800

    ID_EX EX

--- ID_EX.v
+++ ID_EX.v
@@ -26,7 +26,8 @@ module ID_EX{
 };
 
 input clk_i;
-input   WB_i, M_i, EX_i, PC_i, ReadData1_i, ReadData2_i;
+input   [3:0]   EX_i;
+input   WB_i, M_i, PC_i, ReadData1_i, ReadData2_i;
 output  WB_o, M_o, EX_o_1, EX_o_2, EX_o_3;
 input   inst1_i, inst2_i, inst3_i, inst4_i, inst5_i;
 output  inst1_o, inst2_o, inst3_o, inst4_o, inst5_o;
@@ -37,6 +38,9 @@ always(posedge clk_i)begin
     WB_o = WB_i;
     M_o = M_i;
     //TODO EX!!
+    EX_o_1 = EX_i[3];
+    EX_o_2 = EX_i[2:1];
+    EX_o_3 = EX_i[0];
     inst1_o = inst1_i;
     inst2_o = inst2_i;
     inst3_o = inst3_i;
