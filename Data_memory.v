3743739a69a4b11e59ac79ec9322467385708c97
Derek Kuo <ccderek@bitbucket.org>
2015-12-05 01:34:00 +0800

    Data mem

---
+++ Data_memory.v
@@ -0,0 +1,24 @@
+module Data_memory{
+	.MemWrite_i,
+	.MemRead_i,
+	.Addr_i,
+	.WriteData_i,
+	.ReadData_o
+};
+
+// Interface
+input   [31:0]      Addr_i, WriteData_i;
+output  [31:0]      ReadData_o;
+input   MemWrite_i, MemRead_i;
+
+// Data memory
+reg     [31:0]     memory  [0:255];
+
+always @(MemWrite_i or MemRead_i or Addr_i or WriteData_i)begin
+    if(MemWrite_i)
+        memory[Addr_i>>2] = WriteData_i;
+    if(MemRead_i)
+        ReadData_o = memory[Addr_i>>2];  
+end
+endmodule
+
