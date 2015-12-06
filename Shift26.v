36ac3e2af39bf88ca319777625226e1fbea0d037
bigyo <b02902131@csie.ntu.edu.tw>
2015-12-07 01:29:29 +0800

    edit shift26

--- Shift26.v
+++ Shift26.v
@@ -6,9 +6,10 @@ module Shift26(
 input [25:0] data_i;
 output reg [27:0] data_o;
 
-always
-begin
-	data_o[27:2] = data_i[25:0]; 
+
+always@(data_i) begin
+	data_o[27:2] = data_i[25:0];
+	data_o[1:0] = 2'b00;
 end
 
 endmodule
